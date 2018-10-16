9775afc719685b32a602dade0bee01c231ae86fd
sayedsaid <sayedsaid707@gmail.com>
2018-10-16 12:52:29 +0200

    login page design

--- app/src/main/java/com/example/android/androidlogingregisterapp/MainActivity.java
+++ app/src/main/java/com/example/android/androidlogingregisterapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.android.androidlogingregisterapp;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private RelativeLayout relativeLayout;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            relativeLayout.setVisibility(View.VISIBLE);
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        relativeLayout = findViewById(R.id.relative1);
+
+        handler.postDelayed(runnable, 2000);
     }
 }
